40531c12fcda7c6e30fa7b2b1863f80bfb6f2f93
iChun <ohai.iChun@gmail.com>
2023-02-04 20:01:18 +0000

    Update Specification-Version in Manifest. Use the right fabric event.

--- src/main/java/me/ichun/mods/partyparrots/loader/fabric/TwerkHandlerFabric.java
+++ src/main/java/me/ichun/mods/partyparrots/loader/fabric/TwerkHandlerFabric.java
@@ -3,7 +3,7 @@ package me.ichun.mods.partyparrots.loader.fabric;
 import me.ichun.mods.partyparrots.common.core.TwerkHandler;
 import me.ichun.mods.partyparrots.loader.fabric.events.FabricClientEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class TwerkHandlerFabric extends TwerkHandler
 {
@@ -13,6 +13,6 @@ public class TwerkHandlerFabric extends TwerkHandler
         FabricClientEvents.PLAYER_TICK_END.register(this::onPlayerTickEnd);
         ClientTickEvents.END_CLIENT_TICK.register(this::onClientTickEnd);
         FabricClientEvents.CLIENT_LEVEL_LOAD.register(level -> onLevelLoad());
-        ClientLoginConnectionEvents.DISCONNECT.register((handler, client) -> onClientDisconnected());
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> onClientDisconnected());
     }
 }
