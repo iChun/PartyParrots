7533faca57f76bf86d43f3c47cb7d9f04728584b
iChun <ohai.iChun@gmail.com>
2024-06-26 14:56:43 +0100

    Backport 1.19.4 to 1.19.3. Use of iChunUtil's Gradle workspace for this, lots of files moved around/removed.

---
+++ common/src/main/java/me/ichun/mods/partyparrots/common/core/EventHandlerClient.java
@@ -0,0 +1,24 @@
+package me.ichun.mods.partyparrots.common.core;
+
+import me.ichun.mods.partyparrots.common.PartyParrots;
+import me.ichun.mods.partyparrots.mixin.ParrotAccessorMixin;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Parrot;
+
+public abstract class EventHandlerClient
+{
+    public void onRenderLivingPre(LivingEntity living)
+    {
+        if(living instanceof Parrot parrot)
+        {
+            if(parrot.isFlying() && PartyParrots.config.partyFlying ||
+                    !parrot.isFlying() && (!parrot.isInSittingPose() && PartyParrots.config.partyStanding ||
+                            parrot.isInSittingPose() && PartyParrots.config.partySitting)) //isInSittingPose() = isSitting() (1.16.3+ = isSleeping?? [more like client-side flag])
+            {
+                ((ParrotAccessorMixin)parrot).setPartyParrot(true);
+            }
+        }
+    }
+
+    public abstract void registerTwerkHandler();
+}
