9fe1baa22ffa63bf88a126bcaadb8c829f0fe41f
iChun <ohai.iChun@gmail.com>
2024-06-26 03:19:54 +0100

    Backport 1.20.6 to 1.20.4.

--- neoforge/src/main/java/me/ichun/mods/partyparrots/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/partyparrots/loader/neoforge/LoaderNeoForge.java
@@ -10,7 +10,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(PartyParrots.MOD_ID)
@@ -38,6 +38,6 @@ public class LoaderNeoForge extends PartyParrots
         //register config
         config = iChunUtil.d().registerConfig(new Config(), modEventBus);
 
-        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (minecraft, screen) -> new WorkspaceConfigs(screen));
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new WorkspaceConfigs(screen)));
     }
 }
